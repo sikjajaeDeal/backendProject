9012232d2e071602ea3464804223e9223e34aea7
kwanjung <kwanjungkim@naver.com>
2025-07-08 00:02:18 +0900

    feat : JWT 기반 인증을 위한 Security 설정 및 인증 필터 구현
    -JwtAuthenticationFilter 생성
    -CustomUserDetails 구현
    -SecurityConfig 설정

---
+++ beanBa/src/main/java/likelion/beanBa/backendProject/member/security/service/CustomUserDetails.java
@@ -0,0 +1,54 @@
+package likelion.beanBa.backendProject.member.security.service;
+
+import likelion.beanBa.backendProject.member.model.Member;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+public class CustomUserDetails implements UserDetails {
+
+    private final Member member;
+
+    public CustomUserDetails(Member member) {
+        this.member = member;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(() -> "ROLE_"+member.getRole());
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getMemberId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !"N".equals(member.getUseYn());
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return !"N".equals(member.getDeleteYn());
+    }
+}
