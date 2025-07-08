80a477b1e6639e4c89fc8b09211ae1f13b16e4e2
Kim Nam I <71905358+Nam-I@users.noreply.github.com>
2025-07-08 14:30:52 +0900

    refactor: BackendProjectApplication -> @EnableJpaAuditing 추가

--- beanBa/src/main/java/likelion/beanBa/backendProject/BackendProjectApplication.java
+++ beanBa/src/main/java/likelion/beanBa/backendProject/BackendProjectApplication.java
@@ -2,8 +2,10 @@ package likelion.beanBa.backendProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing //createdAt, updatedAt 사용을 위해 어노테이션 추가
 public class BackendProjectApplication {
 
 	public static void main(String[] args) {
