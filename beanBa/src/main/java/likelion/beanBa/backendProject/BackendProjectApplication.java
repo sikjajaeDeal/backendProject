75ef23bd38e9c87b956e40c455f900a6593b0709
Kim Nam I <71905358+Nam-I@users.noreply.github.com>
2025-07-08 14:44:07 +0900

    refactor: BackendProjectApplication 재수정, BaseTime 삭제

--- beanBa/src/main/java/likelion/beanBa/backendProject/BackendProjectApplication.java
+++ beanBa/src/main/java/likelion/beanBa/backendProject/BackendProjectApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing //createdAt, updatedAt 사용을 위해 어노테이션 추가
 public class BackendProjectApplication {
 
 	public static void main(String[] args) {
