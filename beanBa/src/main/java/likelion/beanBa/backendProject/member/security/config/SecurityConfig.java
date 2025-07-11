e1ee2b73e5a6fd4b25ecb1056d51470c42b39237
kwanjung <kwanjungkim@naver.com>
2025-07-11 11:21:57 +0900

    feat : cors 문제 해결을 위한 CorsConfigurationSource 빈 추가

--- beanBa/src/main/java/likelion/beanBa/backendProject/member/security/config/SecurityConfig.java
+++ beanBa/src/main/java/likelion/beanBa/backendProject/member/security/config/SecurityConfig.java
@@ -19,6 +19,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -36,28 +41,32 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable);
+        http
+                .csrf(AbstractHttpConfigurer::disable);
 
-        http.sessionManagement((session -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)));
+        http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement((session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)));
 
         http.authorizeHttpRequests(auth -> auth
-                .requestMatchers(
-                        "/api/auth/login",
-                        "/api/auth/refresh",
-                        "/api/auth/signup/**",
-                        "/oauth2/**",
-                        "/upload",
-                        "/swagger-ui/**",
-                        "/v3/api-docs/**",
-                        "/api/test-sale-post/**"  //sale-post 테스트 하느라고 잠시 넣어놨습니다.
-                ).permitAll()
-                .anyRequest().authenticated()
-        )
-                    .oauth2Login(oauth2 -> oauth2
-                                .userInfoEndpoint(userInfo -> userInfo
-                                        .userService(customOAuth2UserService))
-                                .successHandler(oAuth2LoginSuccessHandler));
+                        .requestMatchers(
+                                "/api/auth/login",
+                                "/api/auth/refresh",
+                                "/api/auth/signup/**",
+                                "/oauth2/**",
+                                "/upload",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/api/test-sale-post/**"  //sale-post 테스트 하느라고 잠시 넣어놨습니다.
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuth2UserService))
+                        .successHandler(oAuth2LoginSuccessHandler));
 
         http.addFilterBefore(
                 new JwtAuthenticationFilter(jwtTokenProvider, customUserDetailsService),
@@ -76,5 +85,18 @@ public class SecurityConfig {
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider(customUserDetailsService);
         return provider;
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("*")); // or List.of("http://localhost:3000") 등
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true); // credentials 필요 없는 경우 false
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
