7f426a0e7bd7c53f4cbbc26d32e6abf39c2d10d1
hwichoi0317 <chowko@naver.com>
2025-07-23 11:45:09 +0900

    웹소켓 관련 설정 수정

--- beanBa/src/main/java/likelion/beanBa/backendProject/chatting/websocket/config/WebSocketConfig.java
+++ beanBa/src/main/java/likelion/beanBa/backendProject/chatting/websocket/config/WebSocketConfig.java
@@ -19,7 +19,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/api/ws-chat") // '/ws-chat' path를 통해 http 요청하여 웹소켓 연결
                 .setHandshakeHandler(new CustomHandshakeHandler())
-                .setAllowedOriginPatterns("*");
+//                .setAllowedOriginPatterns("*");
+                .setAllowedOriginPatterns("http://localhost:8081","https://beanba.store"); // 해당 도메인의 요청만 허락
     }
 
     /*
